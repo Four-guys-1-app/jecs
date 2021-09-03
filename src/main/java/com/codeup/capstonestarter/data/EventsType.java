8800be9726043d95adea31d162b4860284741ab9
Sean Lewis <sean.lewis018@gmail.com>
2021-09-03 14:11:03 -0500

    stuff

---
+++ src/main/java/com/codeup/capstonestarter/data/EventsType.java
@@ -0,0 +1,34 @@
+package com.codeup.capstonestarter.data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "eventTypes")
+public class EventsType {
+    private Long id;
+    private String name;
+
+    public EventsType() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EventsType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/codeup/capstonestarter/web/EventsTypeController.java
@@ -0,0 +1,10 @@
+package com.codeup.capstonestarter.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/eventTypes", headers = "application/JSON")
+public class EventsTypeController {
+
+}
