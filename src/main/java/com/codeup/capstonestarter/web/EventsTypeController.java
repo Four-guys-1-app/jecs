8365c78ee8fe23faf63e054f79d71b9131f7154e
Sean Lewis <sean.lewis018@gmail.com>
2021-09-03 17:38:53 -0500

    fixed

--- src/main/java/com/codeup/capstonestarter/web/EventsTypeController.java
+++ src/main/java/com/codeup/capstonestarter/web/EventsTypeController.java
@@ -1,7 +1,7 @@
 package com.codeup.capstonestarter.web;
 
-import com.codeup.capstonestarter.data.EventsType;
 import com.codeup.capstonestarter.data.EventsTypeRepository;
+import com.codeup.capstonestarter.data.EventsTypes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/eventTypes", headers = "application/JSON")
+@RequestMapping(value = "/api/eventTypes", headers = "Accept=application/json")
 public class EventsTypeController {
 
     private final EventsTypeRepository eventsTypeRepository;
@@ -19,7 +19,7 @@ public class EventsTypeController {
 
 
     @GetMapping
-    private List<EventsType> getEventsByType() {
+    private List<EventsTypes> getEventsByType() {
         return eventsTypeRepository.findAll();
     }
 }
