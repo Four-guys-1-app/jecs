39bcb8e00d66e262edfba71f676a6c8a38f0aff4
kicknback <joshua.borreli@gmail.com>
2021-09-05 00:00:09 -0500

    user seeder

---
+++ src/main/resources/scripts/users_seeder.sql
@@ -0,0 +1,5 @@
+USE capstone_db;
+
+INSERT INTO Users (full_name, username, email, password, bio, postal_code, role) VALUES
+                                                                                     ('sean lewis', 'slew', 'slew@mail.com', 'erogin30g', 'slew is slew', '43820', 'a'),
+                                                                                     ('erik ayala', 'ayye yo', 'yoyo@mail.com', 'w4982h4g9u', 'lettttts goooo', '90387', 'a');
