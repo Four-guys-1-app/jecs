ba1a6df2574f4b7e27c0f8f7811d8c61d224bfe4
Erik Ayala <ayala.erik61@gmail.com>
2021-09-09 10:56:49 -0500

    hi

--- src/main/java/com/codeup/capstonestarter/data/event/EventRepository.java
+++ src/main/java/com/codeup/capstonestarter/data/event/EventRepository.java
@@ -1,6 +1,12 @@
 package com.codeup.capstonestarter.data.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.*;
 
 public interface EventRepository extends JpaRepository <Event, Long> {
+    @Query("from Event e where e.title like %:term%")
+    Event searchByTitleLike(@Param("term") String term);
 }
