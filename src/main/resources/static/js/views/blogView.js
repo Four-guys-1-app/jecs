daaefcfc168d95265e91a145c9ffbafb64058606
Christopher <espinoza.christopher117@gmail.com>
2021-09-13 20:49:20 -0500

    :)

--- src/main/resources/static/js/views/blogView.js
+++ src/main/resources/static/js/views/blogView.js
@@ -1,8 +1,10 @@
 export default function Blog(props) {
     return `
-    <div>
-    hello
-</div>
+    <div class="container" id="blog">
+        <div class="d-flex flex-column flex-wrap align-content-center">
+            <header class="mt-1"><h1>Blog</h1></header>
+        </div>   
+    </div>  
       
    `;
 
