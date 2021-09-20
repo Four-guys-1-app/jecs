6aa89ea7906d80ad16ecfa606619ca0b1b5a73f4
Erik Ayala <ayala.erik61@gmail.com>
2021-09-20 11:46:49 -0500

    login security

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -5,30 +5,36 @@ import createView from "./createView.js";
  * Adds a login event to allow the user to initially obtain a new OAuth2.0 token
  * On a successful response, sets the tokens into storage and redirects to the root
  */
-export default function LoginEvent() {
-    document.querySelector("#auth-user").addEventListener("click", function () {
-        let obj = {
-            username: document.querySelector("#u-username").value,
-            password: document.querySelector("#u-password").value,
-            grant_type: 'password'
-        }
-        let request = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa('rest-blog-client:secret')
-            },
-            body: `grant_type=${obj.grant_type}&username=${obj.username}&password=${obj.password}&client_id=rest-blog-client`
-        };
-        fetchData(
-            {
-                route: `/oauth/token`
-            },
-            request).then((data) => {
-            setTokens(data);
-            createView("/");
+export function LoginEvent() {
+    (function () {
+        document.querySelector("#auth-user").addEventListener("click", function () {
+            let obj = {
+                username: document.querySelector("#u-username").value,
+                password: document.querySelector("#u-password").value,
+                grant_type: 'password'
+            }
+            console.log(obj.username);
+            console.log(obj.password);
+            let request = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + btoa('rest-blog-client:secret')
+                },
+                body: `grant_type=${obj.grant_type}&username=${obj.username}&password=${obj.password}&client_id=rest-blog-client`
+            };
+            fetchData(
+                {
+                    route: `/oauth/token`
+                },
+                request).then((data) => {
+                setTokens(data);
+                createView("/");
+            });
         });
-    });
+
+    })()
+
 }
 
 /**
@@ -51,7 +57,7 @@ export function getHeaders() {
  * Attempts to set the access and refresh tokens needs to authenticate and authorize the client and user
  * @param responseData
  */
-function setTokens(responseData) {
+export function setTokens(responseData) {
     if (responseData.route['access_token']) {
         localStorage.setItem("access_token", responseData.route['access_token']);
         console.log("Access token set");
