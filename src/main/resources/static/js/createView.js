b1849a86f475cdea4b4d419ed2492dae9667e8dd
kicknback <joshua.borreli@gmail.com>
2021-09-27 19:05:26 -0500

    Lot's of important bug work today with Triforce

--- src/main/resources/static/js/createView.js
+++ src/main/resources/static/js/createView.js
@@ -12,6 +12,9 @@ export default function createView(URI) {
 
     let route = router(URI);
 
+    // Store the title because the loading screen render overwrites it.
+    let currentTitle = document.title;
+
     if (!route) {
         render(null, router('/error'));
         return;
@@ -26,9 +29,24 @@ export default function createView(URI) {
     }
 
     fetchData(route.state, request).then((props) => {
+        // Restore the title so that history entries are not all 'Loading...'
+        // I tried using route.uri here instead, but it seems there's an off-by-one bug (https://stackoverflow.com/a/38830794)
+        document.title = currentTitle;
+        // Add the current page to the history stack
+        history.pushState({...props, lastUri: route.uri }, null, route.uri)
         render(props, route);
     });
 }
 
+export function attachHistoryListener() {
+    // When the user hits back in the browser, get the last uri from history and render it (w/ props)
+    window.addEventListener('popstate', (e) => {
+        if (e?.state?.lastUri) {
+            console.log(`Back to ${e.state.lastUri}!`)
+            const { lastUri, ...props } = e.state
+            render(props, router(lastUri))
+        }
+    });
+}
 
 
--- src/main/resources/static/js/views/EventView.js
+++ src/main/resources/static/js/views/EventView.js
@@ -1,13 +1,43 @@
+import getMap, {setMarker} from "../mapbox.js";
+
 export default function EventView(props) {
     return `
     <div class="container">
         
-       <h1>${props.event.title}</h1>
+        <header class="d-flex justify-content-between">
+            <h1 id="event-header" class="mb-4">${props.event.title}</h1>
+            <p>Created on: ${props.event.dateCreated}</p>
+            <p>By: ${props.event.user.username}</p>
+        </header>
+        <hr>
+        <div id="event-view-map" class="my-5 rounded d-flex justify-content-center">
+        </div>
+        <hr>
+        
+        <div class="d-flex justify-content-center">
+            <p>${props.event.description}</p>
+        </div>
+        
+        
+       
+       
+       
     </div>
     `;
 }
 
 
 export function EventViewEvent()  {
+
+    // console.log(props)
+
+    let map = getMap($("#event-view-map").attr("id"));
+
+    map.on('load', () => {
+        map.resize();
+    })
+
+
+
 
 }
