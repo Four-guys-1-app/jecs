89a759353b6e90a6141bbf4b126ee58970ee0c51
kicknback <joshua.borreli@gmail.com>
2021-09-28 21:57:17 -0500

    More work on event view

--- src/main/resources/static/js/views/EventView.js
+++ src/main/resources/static/js/views/EventView.js
@@ -20,8 +20,10 @@ export default function EventView(props) {
         <div class="d-flex justify-content-center mb-4">
             <h2>-Event Comments-</h2>
         </div>
-        <div>
-            <!-- area for comments -->
+        <div class="container justify-content-center mt-5 border-left border-right">
+            <div class="d-flex justify-content-center pt-3 pb-2"> 
+                <input type="text" name="text" placeholder="+ Add a comment" class="form-control addtxt"> 
+            </div>
         </div>
         <div class="d-flex justify-content-center">
             <button class="glow-on-hover">Add comment</button>
@@ -33,11 +35,18 @@ export default function EventView(props) {
     `;
 }
 
+function getCommentsHtml(comments) {
+    return comments.map(comment => `
+                
+                `).join('')
+}
+
+
 
 export function EventViewEvent()  {
 
 
-    let map = getMap($("#event-view-map").attr("id"));
+    let map = getMap($("#event-view-map").attr("id"), 13);
 
     map.on('load', () => {
         map.resize();
